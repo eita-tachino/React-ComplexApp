7f6872b8957e8ff3a5c329aba6302550ffbe8deb
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-11-26 20:54:27 +0900

    06. useContext Section

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Page from './Page'
 import Axios from 'axios'
 import { withRouter } from 'react-router-dom'
+import ExampleContext from '../ExampleContext'
 
 function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
+  const { addFlashMessage } = useContext(ExampleContext)
   async function handleSubmit(e) {
     e.preventDefault()
     try {
@@ -15,6 +17,7 @@ function CreatePost(props) {
         token: localStorage.getItem('complexappToken'),
       })
       // Redirect to new post url
+      addFlashMessage('Congrats, you successfully created a post!')
       props.history.push(`/post/${response.data}`)
       console.log('New post was created.')
     } catch (e) {
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import ExampleContext from '../ExampleContext'
 
 function HeaderLoggedIn(props) {
+  const { setLoggedIn } = useContext(ExampleContext)
   function handleLogout() {
-    props.setLoggedIn(false)
+    setLoggedIn(false)
     localStorage.removeItem('complexappToken')
     localStorage.removeItem('complexappUsername')
     localStorage.removeItem('complexappAvatar')
